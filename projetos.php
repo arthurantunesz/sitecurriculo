9d476fd7a23c1f6e981972b4406d805ac69f8277
Arthur <arthurantunes@usp.br>
2022-03-27 12:31:43 -0300

    refresh

--- projetos.php
+++ projetos.php
@@ -21,13 +21,46 @@
                 <li><a href="./index.html">Contato</a></li>
             </ul>
         </nav>
-
-
-        
     </header>
-    
+        
+    <section class="projetos">
+        <h1>Projetos</h1>
+        <div class="container">
+            <div>
+                <h1>Feira de Empreendedorismo Inovac - 2018</h1>
+                <p>Organizamos uma feira de fomento à inovação e sustentabilidade, contando com a
+                    participação de várias empresas patroninadoras. Nosso evento deu destaque a algumas
+                    empresas júniores no ramo de tecnologia para conseguirem investimento.
+                </p>
+                <p>Como um projeto de conclusão de curso, colocamos em prática conceitos aprendidos em aula.
+                    Sendo assim, fizemos o planejamento do evento e captação de recursos financeiros. Depois,
+                    fizemos a mobilização de esforços de alunos e comunidade local, para apoiar o projeto.
+                    Além disso, fizemos o plano de marketing e toda a organização logística.
+                </p>
+                <p>Conseguimos mais de 3 mil participantes no evento e duas empresas da nossa escola com investimento real.
+                    Também arrecadamos 1,2 toneladas de alimentos para doação.
+                </p>
+                <img src="./assets/Projeto Eniac - Empreendedorismo.jpg" alt="">
+            </div>
 
-
+            <div>
+                <h1>Iniciativa Consorciei Learning - 2021</h1>
+                <p>Eu e uma colega do trabalho organizamos um curso de Excel avançado para
+                    oferecer a todos os colaboradores da empresa, visto a importância dessa ferramenta em suas rotinas de operação.
+                    Nós dois planejamos o cronograma e demos as aulas, duas vezes por semana, durante um mês.
+                </p>
+                <p>Abordamos várias funcionalidades do Excel, como tabelas dinâmicas e criação de dashboards.
+                    Também explicamos o uso do Power Query para importação e atualização de dados.
+                    Por último, praticamos muito o uso do VBA, criando automações e interação com a web.
+                </p>
+                <p>Tivemos quase metade da empresa inscrita no nosso curso e após avaliação final, 
+                    recebemos um feedback muito positivo, o que nos motivou a prosseguir com a iniciativa e 
+                    elaborar novas atividades dentro da empresa para o compartilhamento de conhecimentos e experiências.
+                </p>
+                <img src="./assets/cursoexcel.jpg" alt="">
+            </div>
+        </div>
+    </section>
 
 
 </body>
