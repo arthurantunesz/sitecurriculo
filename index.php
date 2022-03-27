cb675bbafbf558932e7079b24a50bc3f5a945918
Arthur <arthurantunes@usp.br>
2022-03-27 11:55:50 -0300

    refresh

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <html>
+
+    <style>
+        body{
+            color: red;
+        }
+    </style>
+
   <body>
     <h1><?= "Esse é o meu primeiro projeto na web"; ?></h1>
     <h2> Desenvolvido por Alba Lopes </h2>
