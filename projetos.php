3c49c9dc7f2140ff42b0f0e3e361b71a318c218f
Arthur <arthurantunes@usp.br>
2022-03-27 12:27:52 -0300

    refresh

---
+++ projetos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projetos</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <a href="./index.html">Currículo</a>
+        <nav>
+            <ul class="menu">
+                <li><a href="./index.html">Formação</a></li>
+                <li><a href="./index.html">Experiência</a></li>
+                <li><a href="./index.html">Habilidades</a></li>
+                <li><a href="./index.html">Certificações</a></li>
+                <li><a href="./projetos.html">Projetos</a></li>
+                <li><a href="./index.html">Contato</a></li>
+            </ul>
+        </nav>
+
+
+        
+    </header>
+    
+
+
+
+
+</body>
+</html>
